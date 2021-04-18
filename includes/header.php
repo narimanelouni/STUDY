1ffc0002bcb030b89b3e8076909999551e5300ea
poisonlylo <66483872+poisonlylo@users.noreply.github.com>
2021-04-18 15:32:43 +0100

    first commit

---
+++ includes/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>  
+    <link rel="stylesheet" href="css/main-style.css">
+    <link rel="stylesheet" href="css/layout-style.css">
+    <title>STUDY</title>
+</head>
+<body>
+    <div class="header-plus-image">
+        <div class="img-header">
+           <a href=""><img src="/imgs/photo-header.svg" alt=""></a> 
+        </div>
+        <div class="header">
+            <div class="navbar-backg-color">
+                <div class="navbar">
+                    <div class="logo">
+                        <img src="/imgs/logo.png" alt="">
+                    </div>
+                        <div class="navbar-right">
+                            <a href="#nd-page" class="navbar-right-link">A propos</a>
+                            <a href="inscription.html" class="navbar-right-link">S’inscrire</a>
+                            <a href="connexion.html" class="navbar-right-link">Connexion</a>
+                        </div>
+                </div>
+            </div>
+            <div class="presentation">
+                <h1>Trouvez les meilleurs 
+                    enseignants de chez vous !</h1>
+        </div>
+    </div>
+</div>
